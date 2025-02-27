a96ac13c2858a40c800fafcdbcbaf87413306ede
sanwaralkmali <mtnshsalah@gmail.com>
2025-02-27 23:28:48 +0300

    deploy 23

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -17,13 +17,13 @@ const photos = [
   },
   {
     id: 3,
-    src: 'https://media.istockphoto.com/id/149081119/tr/foto%C4%9Fraf/traditional-yemen-houses-sanaa.jpg?s=1024x1024&w=is&k=20&c=4v_ousMWg1m330Zd4Q8FFj3i9wHbOTpaQJjyq8BJYA4=',
+    src: 'https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480528057_937550701864395_6341600704020443735_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=egxgVhApGhEQ7kNvgFXOgd8&_nc_oc=AdiYBNqzKCP74eysIZABnphHS-t1VpFwyR-RoK3jGSWT6g7rdQUoq9OSUixx6O15746apHVe0xvr01tWhMzpxPUA&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AklS5yN19LPXrDrK0oaA0cl&oh=00_AYBG5dQIM1HZeBm9M4amVOELZioaXjR2qhzrEw_oEy7riw&oe=67C684EE',
     alt: 'Old buildings',
     category: 'Sana\'a'
   },
   {
     id: 4,
-    src: 'https://media.istockphoto.com/id/152960953/tr/foto%C4%9Fraf/panorama-of-sanaa-at-night-yemen.jpg?s=1024x1024&w=is&k=20&c=uU_Xtr_G1k-K3OpXmp50u2823uVfl5UIJ5jSpTRvp6k=',
+    src: 'https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480888427_937545318531600_5422307359406453352_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Xj_YpkfBOyoQ7kNvgGn3dLA&_nc_oc=AdiXOCE3kHX4eloe3Vq_tAKWvmKjjZpx76spWhFkgFyGvXhtP_260oeh5QHUkT7vnFQmtqWo-8k5tyQ3ZtKtEYl1&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AcFIkSCMlHjfe1xvvntPKmN&oh=00_AYAB-Yw7fqB5OW5fpOwRjSWimCUP2wpS8Ggt9S9YKGaLRw&oe=67C69FA7',
     alt: 'Sanaa Old City',
     category: 'Sana\'a'
   },
@@ -112,15 +112,15 @@ const photos = [
   ,
   {
     id: 18,
-    src: 'https://media.gettyimages.com/id/582847878/photo/the-republic-of-yemen.jpg?s=1024x1024&w=gi&k=20&c=OkfZ0TdC-RsEEytpqJuXdP0a8Z0zPrIozq74Uao1tPw=',
+    src: 'https://scontent-ist1-2.xx.fbcdn.net/v/t39.30808-6/480552162_937545261864939_53801212804775848_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=KOujLeiHovgQ7kNvgHQPhL9&_nc_oc=AdiWPfzQtzQa22Wv0T3Le_uVTr16Wq0-DiOElqUENnJ4dw_KkGQk4klvEeRoUk0OBHWZI5V0keAz5pxK-YzpxvKY&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=AHlxdZ7Lsdvin5fOMqS3mgI&oh=00_AYCOMU7iP3I_axXtmGorM3Y4FassTDN_Z2JwtBj8t4Mm5A&oe=67C6A9B8',
     alt: 'Taiz City', 
     category: 'Taiz'
   }
   ,
   {
     id: 19,
-    src: 'https://media.gettyimages.com/id/1749068772/photo/white-trimmed-gingerbread-styled-housing-complexes-are-scattered-among-terraced-farm-plots-on.jpg?s=1024x1024&w=gi&k=20&c=IKr0JMRjMOOarDSKoh6UcDmtm02r6HeLLqrx8tyQGyk=',
-    alt: 'Taiz City',
+    src: 'https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480664472_936986708587461_6768056836326528719_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=kIqXauuoQaAQ7kNvgF42UHK&_nc_oc=Adif14mmR0suvNzyC1j9d0pvpo15qKB8SEC3qwDcSTCC0w-p9NKRe_ZhMtz8c9hMDGlbmUiP86znD-bc4ckv4itR&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=A-vfP6N3EfatXtoetjCMev9&oh=00_AYA9q22MxDY0ug5Cns3_nbrsRxk00pPBL0lBhV1UgRdL4Q&oe=67C6B273',
+    alt: 'Yemeni Flag',
     category: 'Nature'
   }
   ,
@@ -135,7 +135,7 @@ const photos = [
     id: 21,
     src: 'https://scontent.fist20-1.fna.fbcdn.net/v/t39.30808-6/480783976_1100718481829232_4528252137738178694_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=zt8brsVMTVgQ7kNvgFG3QYi&_nc_oc=AdjNOibRnvOB1_jVV8KzoVvR83VS1YKCTspNfHZuh9XgJg2rqF1gyR5Hdcuj-FvJ9K8&_nc_zt=23&_nc_ht=scontent.fist20-1.fna&_nc_gid=Ajdd68hBMwUQI2LydvPfUzC&oh=00_AYBdrMuP0j1QV2N8yVbTvmgKR7bPckOlGjyBICp7BUnR_A&oe=67C5DB54',
     alt: ' ',
-    category: ' Nature'
+    category: 'Nature'
   }
   ,
   {
@@ -226,13 +226,13 @@ const photos = [
     id: 35,
     src: "https://i.pinimg.com/736x/37/47/37/37473738358a547af8b3796c9db359da.jpg",
     alt: "Old buildings in Shibam",
-    category: "Shibam"
+    category: "culutral heritage"
   },
   {
     id: 36,
     src: "https://i.pinimg.com/736x/c9/f5/8a/c9f58ab6fb6aeff57a67d95b73371f3b.jpg",
     alt: "Shehara Bridge, an iconic historical landmark in Yemen",
-    category: "Shehara"
+    category: "culutral heritage"
   },
   {
     "id": 37,
@@ -268,9 +268,80 @@ const photos = [
     "id": 42,
     "src": "https://scontent-ist1-2.xx.fbcdn.net/v/t39.30808-6/475564217_922534039995687_3890162233571671186_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=DrHKfwmIWGAQ7kNvgHa3m2t&_nc_oc=AdihM0l6VM11m7I0VQpwRc-XFppPNAjQWDiNSFN7WgNpIS50bSNdJFP-GvDWc7zeaCIUJVzwGGhw2QhWjZ0WElKG&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=AW9sEruwWfCD5o7psfSjCKB&oh=00_AYARo8JvNqJzYTkHZ3LKMhpftBDH5sK52D1QXkp7kSY96A&oe=67C6A9C4",
     "alt": "Yemeni dance",
-    "category": "Culture"
-  }
-    
+    "category": "culture"
+  },
+  {
+    "id": 43,
+    "src": "https://scontent-ist1-2.xx.fbcdn.net/v/t51.75761-15/464967374_17926848140967992_2848010544634995435_n.jpg?stp=dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Zo-yxPUYTiEQ7kNvgFqH1Kj&_nc_oc=AdiyFueXC__KLVXxgQpif2CBXpPmtxB6gwbGADRl5ryQQWjaD09rghiCej_a-kxVDTQUem3Jq5OsH8z1B_IhPtS3&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=A-tSFHYasDyDXrLbDOBdYct&oh=00_AYB-2tAtOTVrFfPVFJPtwR6CBiodJmX7950Ug_I0cD3XNw&oe=67C680B2",
+    "alt": "Socotra Island",
+    "category": "Nature"
+  },
+  {
+    "id": 44,
+    "src": "https://scontent-ist1-2.xx.fbcdn.net/v/t39.30808-6/475410755_921213263498139_2335181635582192727_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=7I3iHkJ2qfsQ7kNvgGY6BmE&_nc_oc=AdjjT-nDwwVZWcz6Q7laho1OjDZYtgFUhkBmRCsehyQxPwqpqTx5b7_x62t803XX8QKwrqspO2VQ78QKj9W9C_sm&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=AaHPxRTJenqGS4g6e3z8jW7&oh=00_AYAqhLS3olOqM9B17EvOhkOmjvBiiibD2a4BUSMh5JaDTw&oe=67C6B096",
+    "alt": "Nature",
+    "category": "Nature"
+  },
+  {
+    "id": 45,
+    "src": "https://scontent-ist1-2.xx.fbcdn.net/v/t39.30808-6/474592967_921213006831498_8210955359195090167_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=BN5VXUYpJyEQ7kNvgH51oit&_nc_oc=AdhdWbxHZ81w5yzZheALQwbE9K4T1b00p9OQtVSXgGmgBVKUEsNwflm4RrL5Dy-NNmnr7jTROvVGBLbThFntnzTD&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=AVs3Jqqfq5rsvUsaNCdw1u2&oh=00_AYA4YcOrnAlp-P9zpDlb0uJshntGuta3i1FhCkwVnRS6xQ&oe=67C69400",
+    "alt": "Yemen beauty",
+    "category": "Nature"
+  },
+  {
+    "id": 46,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/474071227_921212933498172_8217301291591776058_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=54Y5Jy8B3AgQ7kNvgGUenpP&_nc_oc=Adjesig5E8Xh_I7bD2xXZ4YCi6ksdMDux-3hqUGaJjwbefTx8dg4kYg1QdA9H_pa_J_N2MDcFtVL2fZik_FVC4DY&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=A6IhHMZmE_UD7LXAs8WaV0M&oh=00_AYDmStScUpDFGCUFiBTlxNH_sJ84oHUF7CwZHL6Ga4GnYg&oe=67C67C2A",
+    "alt": "Yemen beauty nature",
+    "category": "Nature"
+  },
+  {
+    "id": 47,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/474060455_916595623959903_1645789886960389619_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=5p82rycmLWMQ7kNvgH3W-Is&_nc_oc=AdjLfoWcCqAtq7GTgQ1oKy8R1a2LREXVxHLgQpH6HQhcpgllxSaIbxS5Gh7A7W7SsT3sXx-C35t44j7xF7OizREt&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AT5S2dIX0sd04s87ho8v6YW&oh=00_AYDDRZJt2ywI2ok-vNAxae3k6V0mPKm7U_m0UPOjD2V84g&oe=67C6925C",
+    "alt": "Yemeni art",
+    "category": "Art"
+  },
+  {
+    "id": 48,
+    "src": "https://scontent-ist1-2.xx.fbcdn.net/v/t39.30808-6/471199816_898940629058736_2784181461497819591_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=m9qNVYqjDZsQ7kNvgGV3Gf2&_nc_oc=AdgUXFfJzadcSPthHq8HpyZlRlmJs1XZ1losyE5n_KK6uPT75oFG1ed4RxrzAUj6iJoGfGZZ_yPcya1D0sM51dtF&_nc_zt=23&_nc_ht=scontent-ist1-2.xx&_nc_gid=A8q4qHVe__vMH9QIGmkkiNY&oh=00_AYCKccCNEQtJYYUP5zs7kD4Yb16dyRSL4F5DjMHP3wlMZA&oe=67C6A135",
+    "alt": "Yemeni passport",
+    "category": "culture"
+  },
+  {
+    "id": 49,
+    "src": "  https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480856564_937831971836268_7250752659512081643_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=p7_rsrBCPxUQ7kNvgFDv7Jo&_nc_oc=AdgNpKfCO60B7wMqb-kqIVZBhz5f2b1fqs8gRjfVQC9G37iLb_4cStslDQTmnIbp4l4aatYQfWrcPMLpRnAG3Yui&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AhLaQJPY2MdjkLt5-qUOlPP&oh=00_AYCHT23g6wqRCceLdu6F4RsKIMTDfykftTgc9GzHohFwsQ&oe=67C68BB3",    "alt": "Old buildings",
+    "category": "culture"
+  },
+  {
+    "id": 50,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/481114912_937832115169587_6313731230551505116_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=GXNLOLT8nawQ7kNvgHJgAoJ&_nc_oc=AdhY3zGKFIzVHtFS_2q9cwo4Mqwy_4B7rZxLxe-4bm6FH3pgnRi8BHxxB_DtmmWvPt0o8C5796yZgw7ydJCwUQjD&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AFclwpnliMS1c8m4I3-Bqyt&oh=00_AYCsFy11aromBS1ZLle3NWxdO40DJcpLw3hL2jvmyzfouw&oe=67C6846F",
+    "alt": "Old buildings",
+    "category": "culture"
+  },
+  {
+    "id": 51,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480550935_937593351860130_4969190582837234592_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=KWPNKPWyMFEQ7kNvgFHqjqT&_nc_oc=AdhPDGbRrp3vQqwXZ92TMZzvlH5615Z31CieEal3kjYkLQG-3nobrgJGSIIGA_lAPuHd9yFXqB9MYpKo1BtmyX_8&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AcONrPV--XHo65HBDjcucGI&oh=00_AYDLKWzTax91Sk_aSaLNLrh3OqZK7TTfY2gP5rOYgrfzQw&oe=67C6B4F3",
+    "alt": "Ibb",
+    "category": "nature"
+  },
+  {
+    "id": 52,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/481114912_937832115169587_6313731230551505116_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=GXNLOLT8nawQ7kNvgHJgAoJ&_nc_oc=AdhY3zGKFIzVHtFS_2q9cwo4Mqwy_4B7rZxLxe-4bm6FH3pgnRi8BHxxB_DtmmWvPt0o8C5796yZgw7ydJCwUQjD&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AFclwpnliMS1c8m4I3-Bqyt&oh=00_AYCsFy11aromBS1ZLle3NWxdO40DJcpLw3hL2jvmyzfouw&oe=67C6846F",
+    "alt": "Taiz City",
+    "category": "Taiz"
+  },
+  {
+    "id": 53,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480604076_936977131921752_4048867569649889577_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=8EPpDSD4w1EQ7kNvgEixVBn&_nc_oc=AdjBnbyBCCR1QMb9hl5a5rFDeZu15zGYYOAgWNJwg_nC-MU0Su9B5smj0fmX5rFs8jcolYgZkQHgbHTAwcwUkCFI&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=Azji2-g4lbhK1SMdhbw55YA&oh=00_AYBnxZ1-SwybpYpIjfI1rwJxvjWTCRXWrDIakTxcEcCwag&oe=67C6AC53",
+    "alt": "A stunning view of Aden at night with city lights",
+    "category": "Aden"
+  },
+  {
+    "id": 54,
+    "src": "https://scontent-ist1-1.xx.fbcdn.net/v/t39.30808-6/480656776_936941421925323_2475451009137149415_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=JTEGKhXmnEEQ7kNvgFLWMDb&_nc_oc=AdjxS4tLLEPkhcxQXGVXSw1OkjcqwHxonysgeR-YvFtnyWONMpOKvQYncztCcc11fMq_swAFyvVs7ck4B3gzDoUV&_nc_zt=23&_nc_ht=scontent-ist1-1.xx&_nc_gid=AZvKZwwj6oDgs5gjET8_pDN&oh=00_AYB_COgX5PAx5Z80N0tg0FwMOy8zJk0xzBi4QVtuDQ5Sxg&oe=67C69130",
+    "alt": "A group of Yemeni girls holding Quran",
+    "category": "People"
+  },
+  
 ];
 
 export const PhotoGallery: React.FC = () => {
