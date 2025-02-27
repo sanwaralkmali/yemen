c6c35e197f46e7f9016b1e06afaa993236bc23c3
sanwaralkmali <mtnshsalah@gmail.com>
2025-02-27 09:37:21 +0300

    deploy 7

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -35,7 +35,7 @@ const photos = [
   },
   {
     id: 6,
-    src: '../../public/assets/pictures/6.jpg',
+    src: 'https://plus.unsplash.com/premium_photo-1670689707643-f36b72da4405?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     alt: 'Sanaa Old City',
     category: 'Historical buildings'
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   base: '/yemen/', // Set the base path to match your repository name
 });
